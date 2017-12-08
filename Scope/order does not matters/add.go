95186c364140d01ba1d78acba2bd79788d122ca0
Shivam Nema <nemashivam5@gmail.com>
2017-12-08 19:47:05 +0530

    more learninh

---
+++ Scope/order does not matters/add.go
@@ -0,0 +1,7 @@
+package add
+
+
+var Number = 10;
+func Add(a int, b int) int {
+	return a + b
+}
--- Scope/order does not matters/main.go
+++ Scope/order does not matters/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"add"
+)
 
 func main() {
 	x := 10
 	fmt.Println("Value of X", x)
 	fmt.Println("Value of Y declared outside main func: ", y)
+	sum := Add(10, 230)
+	fmt.Println("Sum of the value: ", Number)
 
 }
 
